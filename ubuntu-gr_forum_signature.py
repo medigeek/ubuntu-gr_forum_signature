20fb39d86c935c862264737a61758ade376031d3
Savvas Radevic <vicedar@gmail.com>
2010-11-24 22:09:31 +0100

    * Bugfix update * Added argument support for testing (lshw xml file)

--- ubuntu-gr_forum_signature.py
+++ ubuntu-gr_forum_signature.py
@@ -40,19 +40,27 @@ import xml.dom.minidom
 import re
 
 class core():
-    def __init__(self):
+    def __init__(self, fxml=""):
         # unicode("Πολύ καλές", "utf-8")
+        self.unknown = u"�" # Character/string for unknown data
         self.lshwxml = ""
-        self.lshwinfo = {}
-        self.lshw()
+        #self.lshwinfo = {}
+        self.lshwinfo = {
+            "cpu": self.unknown,
+            "memory": self.unknown,
+            "display": list(self.unknown),
+            "system": self.unknown,
+            "core": self.unknown,
+        }
+        self.lshw(fxml)
         self.knowledge()
         self.osinfo()
         self.specs()
 
     def knowledge(self):
-        s = {   "linux": u"Άγνωστο",
-                "programming": u"Άγνωστο",
-                "english": u"Άγνωστο"
+        s = {   "linux": self.unknown,
+                "programming": self.unknown,
+                "english": self.unknown
             }
         print( u"Γνώσεις ⇛ Linux: %s ┃ Προγραμματισμός: %s ┃ Αγγλικά: %s" %
                 (s["linux"], s["programming"], s["english"])
@@ -99,82 +107,106 @@ class core():
         elif re.match(r'i686|i386', output):
             mtype = "32-bit"
         else:
-            mtype = "x-bit"
+            mtype = "%s-bit" % self.unknown
         return mtype
 
     def specs(self):
-        print( u'Προδιαγραφές ⇛ %s │ RAM %s MiB │ %s│ Μητρική: %s (%s)' %
+        print( u'Προδιαγραφές ⇛ %s │ RAM %s MiB │ Κάρτα γραφικών: %s │ Μητρική: %s - %s' %
                 (self.lshwinfo["cpu"],
                 self.lshwinfo["memory"],
-                self.lshwinfo["display"],
+                ', '.join(self.lshwinfo["display"]),
                 self.lshwinfo["system"],
                 self.lshwinfo["core"]
                 )
         )
 
-    def cpuinfo(self):
-        print("CPU: %s" % self.lshwinfo["cpu"])
-
-    def display(self):
-        print("Display: %s" % self.lshwinfo["display"])
-
-    def memory(self):
-        print("Memory: %d MiB" % self.lshwinfo["memory"])
-
-    def mainboard(self):
-        print("Mainboard: %s" % self.lshwinfo["system"])
-
-    def lshw(self):
+    def lshw(self, fxml=""):
         """ Runs lshw and sets self.lshwxml variable if not already set """
-        if not self.lshwinfo:
-            if not self.lshwxml:
+        if not self.lshwxml:
+            if fxml:
+                f = open(fxml)
+                self.lshwxml = f.read()
+                f.close()
+                print("\n\nUsing lshw xml file: %s" % fxml)
+            else:
                 self.lshwxml = self.runcommand(["sudo", "lshw", "-xml"])
-                # Fix bug: https://bugs.launchpad.net/bugs/512251
-                lines = self.lshwxml.split("\n")
-                for l in lines:
-                    if re.search(r'id="lastmountpoint"', l):
-                        lines.remove(l)
-                self.lshwxml = '\n'.join(lines)
-            doc = xml.dom.minidom.parseString(self.lshwxml)
-            nodes = doc.getElementsByTagName("node")
-            found_motherboard = False # Use alternative motherboard recognition data
-            for n in nodes:
-                if n.hasAttribute("class") and n.getAttribute("class") == "system":
+            # Fix bug: https://bugs.launchpad.net/bugs/512251
+            lines = self.lshwxml.split("\n")
+            for l in lines:
+                if re.search(r'id="lastmountpoint"', l):
+                    lines.remove(l)
+            self.lshwxml = '\n'.join(lines)
+        doc = xml.dom.minidom.parseString(self.lshwxml)
+        nodes = doc.getElementsByTagName("node")
+        found_motherboard = False # Use alternative motherboard recognition data
+        for n in nodes:
+            if n.hasAttribute("class") and \
+                n.getAttribute("class") == "system" and \
+                n.hasAttribute("handle") and n.getAttribute("handle") == "DMI:0001":
+                #<product>MS-7235</product><vendor>MICRO-STAR INTERNATIONAL CO.,LTD</vendor>
+                #product: G31M-S2L - vendor: Gigabyte Technology Co., Ltd.
+                tempa1 = n.getElementsByTagName("product")[0].childNodes[0].nodeValue
+                tempa2 = n.getElementsByTagName("vendor")[0].childNodes[0].nodeValue
+                if re.match(r'MICRO-STAR INTERNATIONAL', tempa2, re.IGNORECASE):
+                    temp_final2 = "MSI"
+                elif re.match(r'Gigabyte Technology', tempa2, re.IGNORECASE):
+                    temp_final2 = "Gigabyte"
+                elif re.match(r'ASUSTeK Computer', tempa2, re.IGNORECASE):
+                    temp_final2 = "ASUS"
+                elif re.match(r'Intel Corporation', tempa2, re.IGNORECASE):
+                    temp_final2 = "Intel"
+                elif re.match(r'American Megatrends', tempa2, re.IGNORECASE):
+                    temp_final2 = "AMI?"
+                else:
+                    temp_final2 = tempa2
+                if temp_final2 != "To Be Filled By O.E.M." and temp_final2 != "System manufacturer":
+                    found_motherboard = True
+                else:
+                    # Clear default motherboard values
+                    temp_final2 = ""
+                    tempa1 = ""
+                if not temp_final2:
+                    self.lshwinfo["system"] = self.unknown
+                else:
+                    self.lshwinfo["system"] = "%s %s" % (temp_final2, tempa1)
+                #print("MOO: %s %s" % (tempa1, tempa2))
+            
+            if n.hasAttribute("id"):
+
+                if n.getAttribute("id") == "core":
+                    #if not found_motherboard:
                     #<product>MS-7235</product><vendor>MICRO-STAR INTERNATIONAL CO.,LTD</vendor>
                     #product: G31M-S2L - vendor: Gigabyte Technology Co., Ltd.
                     tempa1 = n.getElementsByTagName("product")[0].childNodes[0].nodeValue
                     tempa2 = n.getElementsByTagName("vendor")[0].childNodes[0].nodeValue
                     if re.match(r'MICRO-STAR INTERNATIONAL', tempa2, re.IGNORECASE):
                         temp_final2 = "MSI"
-                    elif re.match(r'Gigabyte Technology Co\., Ltd\.', tempa2, re.IGNORECASE):
+                    elif re.match(r'Gigabyte Technology', tempa2, re.IGNORECASE):
                         temp_final2 = "Gigabyte"
+                    elif re.match(r'ASUSTeK Computer', tempa2, re.IGNORECASE):
+                        temp_final2 = "ASUS"
+                    elif re.match(r'Intel Corporation', tempa2, re.IGNORECASE):
+                        temp_final2 = "Intel"
+                    elif re.match(r'American Megatrends', tempa2, re.IGNORECASE):
+                        temp_final2 = "AMI?"
                     else:
                         temp_final2 = tempa2
-                    if temp_final2 != "To Be Filled By O.E.M.":
+                    if temp_final2 != "To Be Filled By O.E.M." and temp_final2 != "System manufacturer":
                         found_motherboard = True
-                    self.lshwinfo["system"] = "%s %s" % (temp_final2, tempa1)
-                    #print("MOO: %s %s" % (tempa1, tempa2))
-                
-                if n.hasAttribute("id"):
-                    if n.getAttribute("id") == "core":
-                        #if not found_motherboard:
-                        #<product>MS-7235</product><vendor>MICRO-STAR INTERNATIONAL CO.,LTD</vendor>
-                        #product: G31M-S2L - vendor: Gigabyte Technology Co., Ltd.
-                        tempa1 = n.getElementsByTagName("product")[0].childNodes[0].nodeValue
-                        tempa2 = n.getElementsByTagName("vendor")[0].childNodes[0].nodeValue
-                        if re.match(r'MICRO-STAR INTERNATIONAL', tempa2, re.IGNORECASE):
-                            temp_final2 = "MSI"
-                        elif re.match(r'Gigabyte Technology', tempa2, re.IGNORECASE):
-                            temp_final2 = "Gigabyte"
-                        else:
-                            temp_final2 = tempa2
-                        if temp_final2 != "To Be Filled By O.E.M.":
-                            found_motherboard = True
+                    else:
+                        # Clear default motherboard values
+                        temp_final2 = ""
+                        tempa1 = ""
+                    if not temp_final2:
+                        self.lshwinfo["core"] = self.unknown
+                    else:
                         self.lshwinfo["core"] = "%s %s" % (temp_final2, tempa1)
-                        #print("MOO: %s %s" % (tempa1, tempa2))
-                    elif n.getAttribute("id") == "cpu":
+                    #print("MOO: %s %s" % (tempa1, tempa2))
+
+                elif re.match(r'cpu(?::\n)?', n.getAttribute("id")):
+                    try:
                         tempa = n.getElementsByTagName("product")[0].childNodes[0].nodeValue
-                        if tempa[0:5] == "Intel": # Intel(R) Core(TM)2 Duo CPU     E6550  @ 2.33GHz
+                        if tempa[0:5] == "Intel" or tempa[0:7] == "Pentium": # Intel(R) Core(TM)2 Duo CPU     E6550  @ 2.33GHz
                             tempb = re.sub(r'\((?:R|TM)\)|CPU|@', ' ', tempa)
                             temp_final = re.sub('\s{2,}', ' ' , tempb)
                         elif tempa[0:3] == "AMD": # AMD Sempron(tm) Processor LE-1250, AMD Phenom(tm) II X2 550 Processor
@@ -183,22 +215,42 @@ class core():
                         else:
                             temp_final = tempa
                         self.lshwinfo["cpu"] = temp_final
-                    elif n.getAttribute("id") == "display":
-                        tempa1 = n.getElementsByTagName("product")[0].childNodes[0].nodeValue
-                        # G73 [GeForce 7300 GT], Radeon R350 [Radeon 9800 Pro]
-                        tempa2 = n.getElementsByTagName("vendor")[0].childNodes[0].nodeValue
-                        # nVidia Corporation, ATI Technologies Inc
-                        if tempa2[0:6] == "nVidia":
-                            temp_final2 = re.sub(r'(?i)\sCorporation', '', tempa2)
-                        elif tempa2[0:3] == "ATI":
-                            temp_final2 = re.sub(r'(?i)\sTechnologies\sInc', '', tempa2)
-                        else:
-                            temp_final2 = tempa2
-                        self.lshwinfo["display"] = "%s %s" % (temp_final2, tempa1)
-                    elif n.getAttribute("id") == "memory":
-                        tempa = n.getElementsByTagName("size")[0].childNodes[0].nodeValue
-                        temp_final = int(tempa) / 1024**2 # Size in bytes, divide by 1024**2, result in MiB
-                        self.lshwinfo["memory"] = temp_final
+                    except IndexError:
+                        pass
+
+                elif re.match(r'display(?::\n)?', n.getAttribute("id")):
+                    tempa1 = n.getElementsByTagName("product")[0].childNodes[0].nodeValue
+                    # G73 [GeForce 7300 GT], Radeon R350 [Radeon 9800 Pro]
+                    tempa2 = n.getElementsByTagName("vendor")[0].childNodes[0].nodeValue
+                    # nVidia Corporation, ATI Technologies Inc
+                    if tempa2[0:6] == "nVidia":
+                        temp_final2 = re.sub(r'(?i)\sCorporation', '', tempa2)
+                    elif tempa2[0:3] == "ATI":
+                        temp_final2 = re.sub(r'(?i)\sTechnologies\sInc', '', tempa2)
+                    else:
+                        temp_final2 = tempa2
+
+                    displaystr = "%s %s" % (temp_final2, tempa1)
+                    # Remove default data
+                    try:
+                        self.lshwinfo["display"].remove(self.unknown)
+                    except ValueError:
+                        pass
+                    # Create an array with displays
+                    try:
+                        self.lshwinfo["display"].index(displaystr)
+                    except ValueError:
+                        self.lshwinfo["display"].append(displaystr)
+
+                elif re.match(r'memory(?::\n)?', n.getAttribute("id")):
+                    try:
+                        x = n.getElementsByTagName("description")[0].childNodes[0].nodeValue
+                        if x == "System Memory":
+                            tempa = n.getElementsByTagName("size")[0].childNodes[0].nodeValue
+                            temp_final = int(tempa) / 1024**2 # Size in bytes, divide by 1024**2, result in MiB
+                            self.lshwinfo["memory"] = temp_final
+                    except:
+                        pass
 
     def runcommand(self, command):
         p = subprocess.Popen(command, stdout=subprocess.PIPE)
@@ -214,9 +266,12 @@ class core():
         return lines
 
 def main():
-    #print("YO!")
-    core()
-
+    #Argument: lshw xml filename (for testing)
+    try:
+        arg = sys.argv[1]
+    except IndexError:
+        arg = ""
+    core(fxml=arg)
 if __name__ == "__main__":
     main()
 
